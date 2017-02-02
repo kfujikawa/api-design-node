4ec8aaa181c96bd51045f0a18ac584fe1d9f2dc8
Kinuyo Fujikawa <kinuyofujikawa@gmail.com>
2017-02-02 15:06:15 -0800

    commit

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 // TODO: make this work.
-// if yuo go to localhost:3000 the app
+// if you go to localhost:3000 the app
 // there is expected crud to be working here
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -8,12 +8,12 @@ var _ = require('lodash');
 
 // express.static will serve everything
 // with in client as a static resource
-// also, it will server the index.html on the
+// also, it will serve the index.html on the
 // root of that directory on a GET to '/'
 app.use(express.static('client'));
 
 // body parser makes it possible to post JSON to the server
-// we can accss data we post on as req.body
+// we can access data we post on as req.body
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
@@ -22,6 +22,60 @@ var lions = [];
 var id = 0;
 
 // TODO: make the REST routes to perform CRUD on lions
+//GET all lions
+app.get('/lions', function (req, res){
+	res.json(lions);
+});
+
+//GET lion by id (lodash)
+app.get('/lions/:id', function(req, res){
+  var lion = _.find(lions, {id: req.params.id});
+  res.json(lion || {});
+});
+
+//POST create a new lion
+app.post('/lions', function (req, res){
+	var lion = req.body;
+	id++;
+	lion.id = id + '';
+
+	lions.push(lion);
+
+	res.json(lion);
+});
+
+//PUT update matching lion 
+app.put('/lions/:id', function (req, res){
+	var update = req.body;
+
+	if (update.id){
+		delete update.id;
+	}
+
+	var lion = _.find(lions, {id: req.params.id});
+
+	if (!lion[lion]){
+		res.send();
+	} 
+
+	else{
+		var updatedLion = _.assign(lions[lion], update);
+		res.json(updatedLion);
+	}
+});
+
+app.delete('/lions/:id', function(req, res){
+	var lion = _.find(lions, {id: req.params.id});
+
+	if(!lions[lion]){
+		res.send();
+	}
+	else{
+		var deletedLion = lions[lion];
+		lions.splice(lion, 1)
+		res.json(deletedLion);
+	}
+});
 
 app.listen(3000);
 console.log('on port 3000');
